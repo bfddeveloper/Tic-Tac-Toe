35c98c4e546644fb0e74e552e97f1888184c0f7d
bfddeveloper <dicksonbrody@icloud.com>
2022-11-03 09:33:36 -0500

    added tap gesture + cell structure

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,20 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
     var body: some View {
         VStack {
            
             Text("Tic Tac Toe")
             LazyVGrid(columns: Array(repeating: GridItem(.fixed(120), spacing: 15), count: 3), spacing: 15) {
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
-                Text("X")
+                ForEach(0..<9) { index in
+                    ZStack{
+                        Color.blue
+                        Text(moves[index])
+                            .font(.system(size: 90))
+                            .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        moves[index] = "X"
+                    }
+                }
+                
             }
         }
         .preferredColorScheme(.dark)
